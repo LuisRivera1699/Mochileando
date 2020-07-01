a0206687694d32e046a4272ba4df9420963f378b
carlarucoba <20161299@aloe.ulima.edu.pe>
2020-06-30 23:28:52 -0500

    Update AdminLog.ts

--- src/AdminLog.ts
+++ src/AdminLog.ts
@@ -1,19 +1,10 @@
-import * as firebase from 'firebase-admin'
-import * as serviceAccount from "package.json"
+var admin = require("firebase-admin");
 
-const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url
-}
-   
-firebase.initializeApp({
-  credential: firebase.credential.cert(params),
-})
+var serviceAccount = require("C:\Users\Carla Rucoba\Desktop\react\prueba git\Mochileando\mochileando-621d2-firebase-adminsdk-44jd2-7886fd2183.json");
+
+const adm = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://mochileando-621d2.firebaseio.com"
+  });
+
+export { adm };
